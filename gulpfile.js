80bf252eff7ba30f948b3a54261b5e47cb2ba0f6
andrwm <me@andrewmcintee.com>
2015-08-27 15:59:54 -0400

    Pass flags into gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -7,43 +7,11 @@ var gulp            = require('gulp'),
     fs              = require('fs'),
     gulpBowerFiles  = require('gulp-bower-files'),
     deploy          = require('gulp-gh-pages'),
+    gulpif          = require('gulp-if'),
+    argv            = require('yargs').argv,
     wiredep         = require('wiredep').stream;
 
 
-//------- HOTFIX ERROR HANDLING
-// https://github.com/gulpjs/gulp/issues/71#issuecomment-68062782
-var origSrc = gulp.src;
-gulp.src = function () {
-    return fixPipe(origSrc.apply(this, arguments));
-};
-
-function fixPipe(stream) {
-    var origPipe = stream.pipe;
-    stream.pipe = function (dest) {
-        arguments[0] = dest.on('error', function (error) {
-            var nextStreams = dest._nextStreams;
-            if (nextStreams) {
-                nextStreams.forEach(function (nextStream) {
-                    nextStream.emit('error', error);
-                });
-            } else if (dest.listeners('error').length === 1) {
-                throw error;
-            }
-        });
-        var nextStream = fixPipe(origPipe.apply(this, arguments));
-        (this._nextStreams || (this._nextStreams = [])).push(nextStream);
-        return nextStream;
-    };
-    return stream;
-}
-//------- END HOTFIX ERROR HANDLING
-
-// gulp.task('wiredep',['bower-files'], function () {
-//   // del("./src/templates/includes/head.jade",function(err, data){console.log("ahhhhhh")} );
-//   gulp.src('./src/includes/_head.jade')
-//      .pipe(wiredep())
-//      .pipe(gulp.dest('./src/includes'));
-//  });
 
 //---------------------- COMPILE
 
@@ -61,7 +29,9 @@ gulp.task('js', function() {
     .pipe( $.browserify({
       debug: true
     }))
-    // .pipe( $.uglify() )
+    .pipe(
+      gulpif( argv.production, $.uglify() )
+    )
     .pipe( $.rename('main.js'))
     .pipe( gulp.dest('dist/assets/scripts/'));
 });
@@ -75,7 +45,7 @@ gulp.task('images', function() {
 })
 
 gulp.task('root-files', function() {
-  return gulp.src('./src/**/*!(.jade)')
+  return gulp.src('./src/*!(.jade)')
     .pipe(gulp.dest('./dist/'))
 })
 
@@ -138,4 +108,15 @@ gulp.task('deploy', ['build'], function () {
     .pipe(deploy())
 });
 
+//---------------------- GUlP DEFAULT
+
 gulp.task('default', ['serve']);
+
+
+
+
+
+
+
+
+
