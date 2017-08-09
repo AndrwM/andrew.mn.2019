0ffffcb25446dade765d50bdd61f7befc41c0a25
Andrew McIntee <contact@andrew.mn>
2017-08-09 14:44:06 -0400

    Oh my god. v2 ALPHA IS ERADY

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,22 +4,34 @@ $(window).load(function() {
    console.log('%c🚀 Interested in what\'s going on under the hood?', 'color: #6a5ef5');
    console.log('%c   Studio@Andrew.mn', 'font-weight: bold;');
 
-  $("[js-sticky-aside]").stick_in_parent({
-    offset_top: 100,
-  });
+  var breakpoint = function () {
+    if ($('body > [js-breakpoint]').length <= 0) {
+      $('body').append('<div js-breakpoint></div>');
+    }
 
-  var headroom  = new Headroom($("[js-navigation]")[0], {
-    tolerance: {
-      down: 3,
-      up: 10
-    },
-    classes: {
-      initial: "c-navigation",
-      pinned: "c-navigation--pinned",
-      unpinned: "c-navigation--unpinned",
-      top: "c-navigation--top",
-      notTop: "c-navigation--not-top",
-      bottom: "c-navigation--bottom",
-      notBottom: "c-navigation--not-bottom" }
-  }).init();
+    return $('[js-breakpoint]').css('z-index');
+  };
+
+  if ( breakpoint() > 0 ) {
+    $("[js-sticky-aside]").stick_in_parent({
+      offset_top: 100,
+    });
+  }
+
+  if ( breakpoint() > 1 ) {
+    var headroom  = new Headroom($("[js-navigation]")[0], {
+      tolerance: {
+        down: 3,
+        up: 10
+      },
+      classes: {
+        initial: "c-navigation",
+        pinned: "c-navigation--pinned",
+        unpinned: "c-navigation--unpinned",
+        top: "c-navigation--top",
+        notTop: "c-navigation--not-top",
+        bottom: "c-navigation--bottom",
+        notBottom: "c-navigation--not-bottom" }
+    }).init();
+  }
 });
