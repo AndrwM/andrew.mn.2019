5355cc3dd8f711aeb4e8bbe63b112ab832c29779
andrwm <contact@andrew.mn>
2019-09-09 18:03:55 -0400

    Updates

---
+++ precache-manifest.d9bbd14b892ff1cd94a822aaa23280ba.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64f9cd8d8fb6a4d98ba5f38a3fe1040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca3c6f54be8c6faf4b5",
+    "url": "/static/css/2.80bcc607.chunk.css"
+  },
+  {
+    "revision": "ad1d344b1dd6ce72479d",
+    "url": "/static/css/main.88296836.chunk.css"
+  },
+  {
+    "revision": "dca3c6f54be8c6faf4b5",
+    "url": "/static/js/2.490a9ea4.chunk.js"
+  },
+  {
+    "revision": "ad1d344b1dd6ce72479d",
+    "url": "/static/js/main.465fe8d9.chunk.js"
+  },
+  {
+    "revision": "d0717cf0d8e7b2fad83e",
+    "url": "/static/js/runtime~main.50873969.js"
+  },
+  {
+    "revision": "040fb954e9f0006459052f0322cf7654",
+    "url": "/static/media/1.040fb954.png"
+  },
+  {
+    "revision": "dec3a3cc8ea3a22d4d7c2a26870ed846",
+    "url": "/static/media/1.dec3a3cc.png"
+  },
+  {
+    "revision": "00e296ca508c9959ff91e8d0c9500401",
+    "url": "/static/media/2.00e296ca.png"
+  },
+  {
+    "revision": "8c65310473b1c8c6ab935d27f47306c9",
+    "url": "/static/media/2.8c653104.png"
+  },
+  {
+    "revision": "5b93cefca03f350a05ecf3759f1b0d54",
+    "url": "/static/media/2019-folio-preview.5b93cefc.jpg"
+  },
+  {
+    "revision": "4c34abf5150979864e3e504526c86ab4",
+    "url": "/static/media/4.4c34abf5.png"
+  },
+  {
+    "revision": "a4ffbcaa287b9f94d4cd56db32b82ec7",
+    "url": "/static/media/Graphik-Light-Web.a4ffbcaa.woff"
+  },
+  {
+    "revision": "44eb8a383149bac058e7451017078dc8",
+    "url": "/static/media/Graphik-Regular-Web.44eb8a38.woff"
+  },
+  {
+    "revision": "c0da9c77d06766d8dda5de3050ec701c",
+    "url": "/static/media/c-wrapper-mask.c0da9c77.svg"
+  },
+  {
+    "revision": "f1b3a18fc47730ebb864b67ace8db352",
+    "url": "/static/media/components.f1b3a18f.png"
+  },
+  {
+    "revision": "3326d7a35ab236e2b9801643c55e7746",
+    "url": "/static/media/cover-1x.3326d7a3.jpg"
+  },
+  {
+    "revision": "9cb78f3be31155ee9a6c586a60bcfb6c",
+    "url": "/static/media/cover.9cb78f3b.png"
+  },
+  {
+    "revision": "37e6df1bacce221e413731bf30333522",
+    "url": "/static/media/cover1.37e6df1b.png"
+  },
+  {
+    "revision": "8b79a13b36b6a69b2fb4f1b1f5522522",
+    "url": "/static/media/feature-detail.8b79a13b.jpg"
+  },
+  {
+    "revision": "a51867643247ec6916f74ea0ab2e0dfb",
+    "url": "/static/media/front.a5186764.png"
+  },
+  {
+    "revision": "ace41b33ecce98d2a64b9a0411755411",
+    "url": "/static/media/logo-auburn.ace41b33.png"
+  },
+  {
+    "revision": "3eec2046c137570e728fddf119acff37",
+    "url": "/static/media/logo-bmw.3eec2046.png"
+  },
+  {
+    "revision": "09929ddc6881a784c65a49b3aa8ac817",
+    "url": "/static/media/logo-clemson.09929ddc.png"
+  },
+  {
+    "revision": "fa4cbaff0dcc388ed68593b24490c908",
+    "url": "/static/media/logo-depend.fa4cbaff.png"
+  },
+  {
+    "revision": "d22ccaa078e2cfefd27094baaa87911f",
+    "url": "/static/media/logo-echo.d22ccaa0.png"
+  },
+  {
+    "revision": "9010850bd2ee140f226e13e41b476fd6",
+    "url": "/static/media/logo-flatiron.9010850b.png"
+  },
+  {
+    "revision": "cf101acad6370613141a325d0391238b",
+    "url": "/static/media/logo-outdoor.cf101aca.png"
+  },
+  {
+    "revision": "9612e370897d6c272a9a3f77344e7c03",
+    "url": "/static/media/logo-pullups.9612e370.png"
+  },
+  {
+    "revision": "25f9365a2a183bd5ed659ef28230fa8e",
+    "url": "/static/media/logo-ridgid.25f9365a.png"
+  },
+  {
+    "revision": "86b51c44b659555cd2852f9ef09c2e81",
+    "url": "/static/media/logo-ryobi.86b51c44.png"
+  },
+  {
+    "revision": "eb52bcc2f56fb9a1d4f138f7dacd0e0d",
+    "url": "/static/media/logo-sc.eb52bcc2.png"
+  },
+  {
+    "revision": "cd467fccb1924e6a9925677a6f17e794",
+    "url": "/static/media/logo-stok.cd467fcc.png"
+  },
+  {
+    "revision": "63425cb0027b6dcf71db7b90c0485121",
+    "url": "/static/media/logo-tiy.63425cb0.png"
+  },
+  {
+    "revision": "a210df762608b640bf034d63931803c4",
+    "url": "/static/media/logo-ubyk.a210df76.png"
+  },
+  {
+    "revision": "538e9554464e7a8181787af7897e2c3d",
+    "url": "/static/media/mock-injury.538e9554.jpg"
+  },
+  {
+    "revision": "508b237d9dba2415c2395886c962faff",
+    "url": "/static/media/mock-v1-1.508b237d.png"
+  },
+  {
+    "revision": "18035c26c2c9f44d040fb6b99c1744fc",
+    "url": "/static/media/mock-v1-2.18035c26.png"
+  },
+  {
+    "revision": "2ee995d94d8b42e78fb340601b44de11",
+    "url": "/static/media/mock-v1-3.2ee995d9.png"
+  },
+  {
+    "revision": "96720e381442273e69e47ad60e1334dc",
+    "url": "/static/media/mock-v1-4.96720e38.png"
+  },
+  {
+    "revision": "dfe697c82607ad27781cd87ad316eb18",
+    "url": "/static/media/preview.dfe697c8.png"
+  },
+  {
+    "revision": "bb9ab12a632f6add83405669cccb1594",
+    "url": "/static/media/preview1.bb9ab12a.jpg"
+  },
+  {
+    "revision": "d01c605781d8a19055e8d47ad607978b",
+    "url": "/static/media/preview1.d01c6057.jpg"
+  },
+  {
+    "revision": "e473f71a18711c3ae2437fe42efdba02",
+    "url": "/static/media/project1.e473f71a.png"
+  },
+  {
+    "revision": "c0d4acba18838a8937f001e61c06fae9",
+    "url": "/static/media/sidebar-1.c0d4acba.png"
+  },
+  {
+    "revision": "a3c24eb0b81c810bbb6f0c0828dd5ae5",
+    "url": "/static/media/signature2.a3c24eb0.png"
+  },
+  {
+    "revision": "5b3134b0219fc112d189fc044be68161",
+    "url": "/static/media/slide-alternates.5b3134b0.png"
+  },
+  {
+    "revision": "9803213a44b866c7c771fed8a5423875",
+    "url": "/static/media/slide-base.9803213a.png"
+  },
+  {
+    "revision": "f56630d4ebd85eb333cdaae1a6bc8b1e",
+    "url": "/static/media/slide-compositions.f56630d4.png"
+  },
+  {
+    "revision": "cf318aaff27705b7fcb6e67f8e3726eb",
+    "url": "/static/media/slide-input.cf318aaf.png"
+  },
+  {
+    "revision": "53eb53c7805b821ae8aa81ecc1fc0410",
+    "url": "/static/media/slide-navigation.53eb53c7.png"
+  },
+  {
+    "revision": "bfb5cdb15528d45884f8688e5bb81370",
+    "url": "/static/media/slide-type.bfb5cdb1.png"
+  },
+  {
+    "revision": "dcd4e51752eae89eb54386a04bca1cf3",
+    "url": "/static/media/slide1.dcd4e517.png"
+  },
+  {
+    "revision": "5d65f8b4c945e5f052cfa45012153028",
+    "url": "/static/media/slide2.5d65f8b4.png"
+  },
+  {
+    "revision": "416425c6880408dd54711c7e59cec78b",
+    "url": "/static/media/slide3.416425c6.png"
+  },
+  {
+    "revision": "6f3eb46f341d9a75768478d2e412fea9",
+    "url": "/static/media/slide4.6f3eb46f.png"
+  },
+  {
+    "revision": "8870e00ee38941d4ff783331b9321379",
+    "url": "/static/media/slide5.8870e00e.png"
+  },
+  {
+    "revision": "8eb0f9d276a57c7762cc6f2fac4a16a2",
+    "url": "/static/media/v2-1.8eb0f9d2.png"
+  },
+  {
+    "revision": "19e22a930f4541f9630bc7a86b25915c",
+    "url": "/static/media/v2-2.19e22a93.png"
+  },
+  {
+    "revision": "4d8b502fe7594fd4871f027ec166a119",
+    "url": "/static/media/v2-3.4d8b502f.png"
+  },
+  {
+    "revision": "ed800dfe1c3624ab4f7e5adfc9afce81",
+    "url": "/static/media/v2-4.ed800dfe.png"
+  },
+  {
+    "revision": "cb9340f87210ef230a1e0d6fe51068a2",
+    "url": "/static/media/v2-5.cb9340f8.png"
+  },
+  {
+    "revision": "8424920552c2cb7f2e1745c03fa22501",
+    "url": "/static/media/v2-6.84249205.png"
+  },
+  {
+    "revision": "f92590718f51d2269ec453d65a167670",
+    "url": "/static/media/v2-7.f9259071.png"
+  },
+  {
+    "revision": "d25689b0c5c1a500b4707f453b082408",
+    "url": "/static/media/whiteboard.d25689b0.jpg"
+  },
+  {
+    "revision": "8e944515f283513f6feb4a823b605eb2",
+    "url": "/static/media/wireframe-v1-1.8e944515.png"
+  },
+  {
+    "revision": "c11365db81ccd2e084c26d25cf648469",
+    "url": "/static/media/wireframe-v1-2.c11365db.png"
+  },
+  {
+    "revision": "0333a54d0d64eaea90dffb5b0c7b86af",
+    "url": "/static/media/wireframe-v1-3.0333a54d.png"
+  },
+  {
+    "revision": "08ccd2f33baf467aaa51115d532ae927",
+    "url": "/static/media/wireframe-v1-4.08ccd2f3.png"
+  },
+  {
+    "revision": "9dc52f88b1f633a1205a12877d74d807",
+    "url": "/static/media/wireframe-v1-5.9dc52f88.png"
+  }
+]);
