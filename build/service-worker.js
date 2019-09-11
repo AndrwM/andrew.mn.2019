9ad1774365a7672da8d3e5479a1c07760e625031
andrwm <contact@andrew.mn>
2019-09-11 18:40:13 -0400

    image update, deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc02c305ee500b90ef2b50071e6ed004.js"
+  "/precache-manifest.23a276d540fff59c8b2db390ea3e291c.js"
 );
 
 self.addEventListener('message', (event) => {
