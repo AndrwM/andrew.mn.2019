1379a76687cecc1080af915a11bcb28230a762d6
andrwm <contact@andrew.mn>
2019-09-12 23:06:00 -0400

    tweaks, deploy

--- src/pages/partials/ExperienceEngineering.js
+++ src/pages/partials/ExperienceEngineering.js
@@ -4,7 +4,7 @@ class ExperienceDesign extends Component {
   render() {
     return (
       <ul className="c-list c-list--large c-list--measure">
-        <li className="c-list__item">HTML/Jade/Mustache/JSX, React/Redux, Angular</li>
+        <li className="c-list__item">HTML/Jade/Mustache/JSX, React/Redux</li>
         <li className="c-list__item">CSS Organization Strategies: BEM, Atomic, Web Components...</li>
         <li className="c-list__item">Ruby On Rails, Elixir+Phoenix, Node, PHP</li>
         <li className="c-list__item">Tooling &amp; Testing: Webpack (&hellip;and predecessors like Gulp), Rspec/Capybara, Jest/Enzyme</li>
